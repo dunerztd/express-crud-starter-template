5f30d5d81595e4caaeda4b4de1ffa3eeec697d79
Alex Baulderstone <46916203+ABaulderstone@users.noreply.github.com>
2020-10-06 09:40:41 +1030

    Initialize challenge-complete

---
+++ models/post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define Post schema
+const Post = new Schema({
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+		minlength: 1
+    },
+    create_date: {
+        type: Date,
+        required: [true, "Create date is required"]
+    },
+    modified_date: {
+        type: Date,
+        required: [true, "Modified date is required"]
+    },
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+		minlength: 1
+    },
+    content: {
+        type: String,
+        required: [true, "Content is required"],
+		minlength: [2, "Content must contain at least two characters"],
+		maxlength: [10240, "Exceeded maximum content length of 10240 characters"]
+    },
+    category: String
+});
+
+module.exports = mongoose.model('Post', Post);
