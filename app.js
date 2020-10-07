aea4ef26d66507384def74ddff57f0eb0545ebe5
Nandini Nayak <nandini.r.nayak@gmail.com>
2020-10-07 14:00:04 +1100

    mongo challenge

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 // const cors = require('cors');
 const mongoose = require('mongoose');
 const postRouter = require('./routes/posts_routes');
+const fetch = require("node-fetch")
+const fs = require('fs')
+const Post = require('./models/post');
+
 
 const port = process.env.port || 3009;
 
@@ -10,7 +14,7 @@ const app = express();
 // bodyparser middle ware built into express parses the req.body
  app.use(express.json());
 
-const dbConn = 'mongodb://localhost/blog_app'
+const dbConn = 'mongodb://localhost/blog_mongo_app'
 // Set three properties to avoid deprecation warnings:
 // useNewUrlParser: true
 // useUnifiedTopology: true
@@ -27,6 +31,53 @@ mongoose.connect(dbConn, {
             console.log('Connected to database!');
         }
     });
+
+
+const fetchData = async () => {
+    try{
+        const dataUrl = 'https://jsonplaceholder.typicode.com/comments'
+        const response = await fetch(dataUrl)
+        const data = await response.json()
+        let json = JSON.stringify( data, null, 2 );
+        
+        // fs.writeFile('comments.json', json, 'utf8', (err) =>{
+        //     if (err) throw err;
+        //     console.log('Data Written to file')
+        // });
+        // let newPost = new Post({ title: 'test' });
+        // await newPost.save();
+        // Post.findOne({title: 'test'}).exec((err, post)=>{
+        //     if(err){
+        //         console.log(err)
+        //     }else{
+        //         console.log(json)
+        //         console.log(post)
+        //         // post.InsertMany(json).exec((err,post)=>{
+        //         //     if(err){
+        //         //         console.log(err)
+        //         //     }else {
+        //         //         console.log("success")
+        //         //     }
+        //         // })
+        //         // console.log("suucess")
+        //     }
+        // })
+        Post.insertMany(json, function(err, docs){
+            if(err){
+                console.log(err)
+            }else{
+                console.log("SUCCESS")
+                console.log(docs)
+            }
+        })
+
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+fetchData();
+
 
 app.get('/', (req, res) => {
     console.log("get on /");
--- models/post.js
+++ models/post.js
@@ -3,31 +3,45 @@ const Schema = mongoose.Schema;
 
 // Define Post schema
 const Post = new Schema({
-    title: {
-        type: String,
-        required: [true, "Title is required"],
-		minlength: 1
-    },
-    create_date: {
-        type: Date,
-        required: [true, "Create date is required"]
-    },
-    modified_date: {
-        type: Date,
-        required: [true, "Modified date is required"]
-    },
-    username: {
-        type: String,
-        required: [true, "Username is required"],
-		minlength: 1
-    },
-    content: {
-        type: String,
-        required: [true, "Content is required"],
-		minlength: [2, "Content must contain at least two characters"],
-		maxlength: [10240, "Exceeded maximum content length of 10240 characters"]
-    },
-    category: String
+    postId:{type: String},
+    id: {type: String},
+    name: {type: String},
+    email: {type: String},
+    body: {type: String}
+
+    
+    // title: {
+    //     type: String,
+    //     required: [true, "Title is required"],
+	// 	minlength: 1
+    // },
+    // create_date: {
+    //     type: Date,
+    //     required: [true, "Create date is required"]
+    // },
+    // modified_date: {
+    //     type: Date,
+    //     required: [true, "Modified date is required"]
+    // },
+    // username: {
+    //     type: String,
+    //     required: [true, "Username is required"],
+	// 	minlength: 1
+    // },
+    // content: {
+    //     type: String,
+    //     required: [true, "Content is required"],
+	// 	minlength: [2, "Content must contain at least two characters"],
+	// 	maxlength: [10240, "Exceeded maximum content length of 10240 characters"]
+    // },
+    // category: String,
+    // comments:[
+    //     {
+    //         name: String,
+    //         email: String,
+    //         body: String
+    //     }
+    // ]
 });
 
 module.exports = mongoose.model('Post', Post);
